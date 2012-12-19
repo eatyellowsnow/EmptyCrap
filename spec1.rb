9299ad2d926a3fdce23de026a4b5cd9bd0d5717c
eatyellowsnow <stepasalin@yandex.ru>
2012-12-19 16:39:26 +0400

    Update spec1.rb

--- spec1.rb
+++ spec1.rb
@@ -1,4 +1,13 @@
-class User 
-end
-describe User do
-end
+require 'rspec'
+describe User1 do
+  before(:all) do
+    @user1 = User1.new("Stepa")
+  end
+  
+  it 'has a name' do
+    @user1.name.should eq "Stepa"
+  end
+  
+end  
+
+
