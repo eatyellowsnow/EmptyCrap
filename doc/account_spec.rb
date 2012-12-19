0249945401d01fc7b14d378eb348082d2d548e72
eatyellowsnow <stepasalin@yandex.ru>
2012-12-19 07:55:11 -0800

    уп ца ыщ ца
    фывфывфывфыыв

---
+++ doc/account_spec.rb
@@ -0,0 +1,26 @@
+require "./account.rb"
+require "./user.rb"
+require 'rspec'
+
+describe Account do
+  before(:all) do
+    @user = User.new("Vasya", "Ivanov", 18)
+    @account = @user.account
+  end
+
+  it 'belongs to the user' do
+    @account.user.should be(@user)
+  end
+
+  it 'has list of accounts' do
+    Account.all.should include(@account)
+  end
+
+  it "should has an ID" do
+    @account.id.should_not be_false
+  end
+
+  it "has a balance in the start" do
+    @account.balance.should be 0
+  end
+end
