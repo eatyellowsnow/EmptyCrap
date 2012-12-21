f3aa017dc5c85ac3dd6600262a05eaf91f61d74b
eatyellowsnow <stepasalin@yandex.ru>
2012-12-21 13:22:52 +0400

    Update doc/account_spec.rb

--- doc/account_spec.rb
+++ doc/account_spec.rb
@@ -8,7 +8,7 @@ driver = Selenium::WebDriver.for :firefox
 driver.navigate.to "http://google.com"
 
 element = driver.find_element(:name,'q')
-element.send_keys "KSDJSDKSDJ!"
+element.send_keys "new text!"
 element.submit
 
 driver.quit
