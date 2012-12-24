932a717a4157d9c79f65356057cdbb743991e0b1
eatyellowsnow <stepasalin@yandex.ru>
2012-12-24 17:50:08 +0400

    сейчас снова должен проходить

--- doc/account_spec.rb
+++ doc/account_spec.rb
@@ -5,7 +5,7 @@ require "rubygems"
 require "selenium-webdriver"
 
 driver = Selenium::WebDriver.for :firefox
-driver.navigate.to "http://google39323929.com"
+driver.navigate.to "http://google.com"
 
 element = driver.find_element(:name,'q')
 element.send_keys "new text!"
