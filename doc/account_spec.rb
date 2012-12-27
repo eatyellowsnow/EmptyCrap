698ac946f30504cb99070e3688e6639aea01032c
eatyellowsnow <stepasalin@yandex.ru>
2012-12-27 07:28:50 +0400

    под дом переправил

--- doc/account_spec.rb
+++ doc/account_spec.rb
@@ -6,7 +6,7 @@ require "selenium-webdriver"
 
 driver = Selenium::WebDriver.for :firefox
 driver.manage.timeouts.implicit_wait = 30
-driver.navigate.to "http://172.18.8.251:3000/#main"
+driver.navigate.to "http://0.0.0.0:3000"
 sleep(30)
 
 
